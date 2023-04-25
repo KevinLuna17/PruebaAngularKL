09ffa89853a067392a193b1ed3b730a0cd52e1d8
KevinLuna17 <kelugo_14@hotmail.com>
2023-04-25 00:19:30 -0500

    Creando componente pantalla-examen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { PantallaExamenComponent } from './components/pantalla-examen/pantalla-examen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    PantallaExamenComponent
   ],
   imports: [
     BrowserModule
